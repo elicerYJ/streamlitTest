e805cac8445668832fb837d7f56fbfe38468cd20
elicer <elicer@yongjinkim.local>
2023-06-12 17:35:32 +0900

    update index.py

--- index.py
+++ index.py
@@ -8,4 +8,8 @@ chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
 
+st.write("## Random data")
+st.dataframe(chart_data)
+
+st.write("## Line chart")
 st.line_chart(chart_data)
